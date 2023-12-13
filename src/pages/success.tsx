5125fddd16eba0e939ad79df5240c5fc6deffeb3
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2023-12-12 22:16:25 -0300

    feat: Estrutura de sucesso

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,3 +1,20 @@
+import { ImageContainer, SuccessContainer } from '@/styles/pages/success'
+import Link from 'next/link'
+
 export default function Success() {
-  return <h1>Product</h1>
+  return (
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer></ImageContainer>
+
+      <p>
+        Uhuul <strong>Jonatán Paes</strong>, sua{' '}
+        <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua
+        casa.
+      </p>
+
+      <Link href="/">Voltar ao catálogo</Link>
+    </SuccessContainer>
+  )
 }
